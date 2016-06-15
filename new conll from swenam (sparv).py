a3490c5799651f6bf2dbec2f45cafa8b2bd53a13
tjarles <chafo246@student.liu.se>
2016-06-15 19:32:53 +0200

    Update new conll from swenam (sparv).py

--- new conll from swenam (sparv).py
+++ new conll from swenam (sparv).py
@@ -1,39 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Manual changes in data:
-#   file  change  prev
-#   634119.htm.utf8.txt   20/6   20 / 6
-#   122420.htm.utf8.txt   Lee "Strach" Perry   Lee " Strach " Perry
-#   122420.htm.utf8.txt   Lee "Strach" Perry   Lee " Strach " Perry
-#   0108233622985_NOJ__00.html.utf8.txt   1994 - 1999   1994-1999
-#   0108233622985_NOJ__00.html.utf8.txt   1990 - 1999   1990-1999
-#   2028.htm.utf8.txt   6/2000   6 / 2000
-#   667344.htm.utf8.txt   23/7 2000   23 / 7 2000
-#   648573.htm.utf8.txt   5/7 2000   5 / 7 2000
-#   0108242712203_NOJ__00.html.utf8.txt   Pearl Jam's   Pearl Jam ' s
-#   675144.htm.utf8.txt   30/7 2000   30 / 7 2000
-#   0108242712203_NOJ__00.html.utf8.txt   aftonbladet.se   turné.aftonbladet.se
-#   675144.htm.utf8.txt   13/7 2000   13 / 7 2000
-#   0108236748703_UTR__00.html.utf8.txt   Milosevic   Milosevic.&amp;nbsp
-#   599413.htm.utf8.txt   10 juni . Milosevic   10 juni.Milosevic
-#   124723.htm.utf8.txt   vinnare : Pauline   vinnare:Pauline
-#   675144.htm.utf8.txt   30/7 2000 Två   30/7 2000Två
-#   599678.htm.utf8.txt   Per Björntorp. Kännetecknande   Per Björntorp.Kännetecknande
-#   655374.htm.utf8.txt   11/7 2000   11 / 7 2000
-#   0108243465791_UTR__00.html.utf8.txt   S:t George's sjukhus   S:t George ' s sjukhus
-
-# Goldstandard changes
-#   file  change
-#   34336.htm.utf8.txt   removed one "Livade Ord"
-#   598207.htm.utf8.txt   moved first "FN:s" one step to the right as it occurs later in the text
-#   658088.htm.utf8.txt   removed one Estonia too many.
-#   605262.htm.utf8.txt   Switched orders to Karlsson, Sven-Erik Karlsson
-#   Removed all newlines inside tags with (\w)\n\, (\w) to \1\, \2
-#   0108236748703_UTR__00.html.txt   moved Drakovic from company to names
-
-
-
-
 # Shellscript for using the tokenization provided by Språkbankens tool Sparv. Make sure the files to be uploaded are in utf-8 (use iconv for quickly encoding multiple files).
 # for f in ../text_files/utf8/*; do curl -X POST -F files[]=@"/Volumes/320/tjarles/Google Drive/729A97/Projekt/SweNam-100-test-files+answer_file/text_files/utf8/${f##*/}" https://ws.spraakbanken.gu.se/ws/sparv/v1/upload? > data.tmp;  grep -E "link" data.tmp | sed -E "s/.*link='(.*)'.*/\1/g" | xargs curl > tmp.zip; unzip tmp.zip; done
 
